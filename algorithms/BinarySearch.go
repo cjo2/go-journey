534c1b32b57bc07553665598c31c13978a24c371
Christopher Docuyanan <christophejd@gmail.com>
2022-02-13 16:50:03 -0800

    Updated language for accuracy - slices, not arrays

--- algorithms/BinarySearch.go
+++ algorithms/BinarySearch.go
@@ -1,14 +1,14 @@
 package algorithms
 
-// BinarySearch executes binary search on a sorted integer array with unique values
+// BinarySearch executes binary search on a sorted integer slice with unique values
 // BinarySearch will return the index of the integer or -1 if the integer cannot be found
-func BinarySearch(array []int, value int) int {
+func BinarySearch(slice []int, value int) int {
 	left := 0
-	right := len(array) - 1
+	right := len(slice) - 1
 
 	for left <= right {
 		middle := (right + left) / 2
-		currentVal := array[middle]
+		currentVal := slice[middle]
 
 		if currentVal == value {
 			return middle
--- algorithms/BinarySearch_test.go
+++ algorithms/BinarySearch_test.go
@@ -7,28 +7,28 @@ import (
 
 func TestBinarySearch(t *testing.T) {
 
-	// BinarySearch when array is of size 1
-	array := []int{0}
-	index := BinarySearch(array, 0)
+	// BinarySearch when slice is of size 1
+	slice := []int{0}
+	index := BinarySearch(slice, 0)
 	assert.Equal(t, 0, index)
 
-	// BinarySearch when array is of size 2 and value is on left
-	//array = []int{0, 1}
-	//index = BinarySearch(array, 1)
+	// BinarySearch when slice is of size 2 and value is on left
+	//slice = []int{0, 1}
+	//index = BinarySearch(slice, 1)
 	//assert.Equal(t, 1, index)
 
-	// BinarySearch when array is of size 2 and value is on right
-	//array = []int{0, 1}
-	//index = BinarySearch(array, 0)
+	// BinarySearch when slice is of size 2 and value is on right
+	//slice = []int{0, 1}
+	//index = BinarySearch(slice, 0)
 	//assert.Equal(t, 0, index)
 
-	// BinarySearch when array is of size 3 and value is in the middle
-	//array = []int{0, 1, 2}
-	//index = BinarySearch(array, 1)
+	// BinarySearch when slice is of size 3 and value is in the middle
+	//slice = []int{0, 1, 2}
+	//index = BinarySearch(slice, 1)
 	//assert.Equal(t, 1, index)
 
 	// Return -1 if the value cannot be found
-	array = []int{1}
-	index = BinarySearch(array, 2)
+	slice = []int{1}
+	index = BinarySearch(slice, 2)
 	assert.Equal(t, -1, index)
 }
