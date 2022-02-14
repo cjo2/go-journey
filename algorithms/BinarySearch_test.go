cd7b3468c1a94c0d2796b5123ee6d2b92a52bed4
Christopher Docuyanan <christophejd@gmail.com>
2022-02-13 16:52:33 -0800

    Accidently left tests for BinarySearch commented out! This uncomments those tests

--- algorithms/BinarySearch_test.go
+++ algorithms/BinarySearch_test.go
@@ -13,19 +13,19 @@ func TestBinarySearch(t *testing.T) {
 	assert.Equal(t, 0, index)
 
 	// BinarySearch when slice is of size 2 and value is on left
-	//slice = []int{0, 1}
-	//index = BinarySearch(slice, 1)
-	//assert.Equal(t, 1, index)
+	slice = []int{0, 1}
+	index = BinarySearch(slice, 1)
+	assert.Equal(t, 1, index)
 
 	// BinarySearch when slice is of size 2 and value is on right
-	//slice = []int{0, 1}
-	//index = BinarySearch(slice, 0)
-	//assert.Equal(t, 0, index)
+	slice = []int{0, 1}
+	index = BinarySearch(slice, 0)
+	assert.Equal(t, 0, index)
 
 	// BinarySearch when slice is of size 3 and value is in the middle
-	//slice = []int{0, 1, 2}
-	//index = BinarySearch(slice, 1)
-	//assert.Equal(t, 1, index)
+	slice = []int{0, 1, 2}
+	index = BinarySearch(slice, 1)
+	assert.Equal(t, 1, index)
 
 	// Return -1 if the value cannot be found
 	slice = []int{1}
