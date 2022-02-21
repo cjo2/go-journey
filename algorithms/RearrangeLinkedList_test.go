f617b1f66b6d9697acc7befe871875b2b6ab1b78
Christopher Docuyanan <christophejd@gmail.com>
2022-02-21 09:00:25 -0800

    Adds Rearrange() even length test

--- algorithms/RearrangeLinkedList_test.go
+++ algorithms/RearrangeLinkedList_test.go
@@ -14,6 +14,7 @@ func TestRearrange(t *testing.T) {
 	l.Append(&data_structures.Node{Value: 4})
 	l.Append(&data_structures.Node{Value: 5})
 
+	// Odd number length
 	node, _ := l.GetNth(0)
 	assert.Equal(t, 1, node.Value)
 	node, _ = l.GetNth(1)
@@ -37,4 +38,32 @@ func TestRearrange(t *testing.T) {
 	assert.Equal(t, 4, node.Value)
 	node, _ = l.GetNth(4)
 	assert.Equal(t, 3, node.Value)
+
+	// Even number length
+	l = data_structures.LinkedList{}
+	l.Append(&data_structures.Node{Value: 1})
+	l.Append(&data_structures.Node{Value: 2})
+	l.Append(&data_structures.Node{Value: 3})
+	l.Append(&data_structures.Node{Value: 4})
+
+	node, _ = l.GetNth(0)
+	assert.Equal(t, 1, node.Value)
+	node, _ = l.GetNth(1)
+	assert.Equal(t, 2, node.Value)
+	node, _ = l.GetNth(2)
+	assert.Equal(t, 3, node.Value)
+	node, _ = l.GetNth(3)
+	assert.Equal(t, 4, node.Value)
+
+	Rearrange(l)
+
+	node, _ = l.GetNth(0)
+	assert.Equal(t, 1, node.Value)
+	node, _ = l.GetNth(1)
+	assert.Equal(t, 4, node.Value)
+	node, _ = l.GetNth(2)
+	assert.Equal(t, 2, node.Value)
+	node, _ = l.GetNth(3)
+	assert.Equal(t, 3, node.Value)
+
 }
