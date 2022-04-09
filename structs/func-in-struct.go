7a370def0869cbfcfdf66929bf698cf7b4b16bbf
Christopher Docuyanan <christophejd@gmail.com>
2022-04-09 15:54:13 -0700

    Adds more detail to structs playground

--- structs/func-in-struct.go
+++ structs/func-in-struct.go
@@ -11,15 +11,15 @@ import (
  */
 
 func main() {
-	porscheTaycan := NewCar("Porsche", "Taycan")
-	porsche718 := NewCar("Porsche", "718")
+	porscheTaycan := NewCar("Porsche", "Taycan", "Atlanta")
+	porsche718 := NewCar("Porsche", "718", "Atlanta")
 	porscheMacan := NewCarStuttgart("Porsche", "Macan", time.Now)
 	porsche991 := NewCarStuttgart("Porsche", "991", GetTime)
 
-	fmt.Printf("func address: %p \n", porscheTaycan.Manufactured)
-	fmt.Printf("func address: %p \n", porsche718.Manufactured)
-	fmt.Printf("func address: %p \n", porscheMacan.Manufactured)
-	fmt.Printf("func address: %p \n", porsche991.Manufactured)
+	fmt.Printf("func address: %p \n", porscheTaycan.ManufacturedTime)
+	fmt.Printf("func address: %p \n", porsche718.ManufacturedTime)
+	fmt.Printf("func address: %p \n", porscheMacan.ManufacturedTime)
+	fmt.Printf("func address: %p \n", porsche991.ManufacturedTime)
 	//func address: 0x107a880
 	//func address: 0x107a880
 	//func address: 0x107a880
@@ -27,24 +27,27 @@ func main() {
 }
 
 type Car struct {
-	Manufactured func() time.Time
-	Make         string
-	Model        string
+	ManufacturedTime     func() time.Time
+	ManufacturedLocation string
+	Make                 string
+	Model                string
 }
 
-func NewCar(carMake string, model string) *Car {
+func NewCar(carMake string, model string, manufacturedLocation string) *Car {
 	return &Car{
-		Manufactured: time.Now,
-		Make:         carMake,
-		Model:        model,
+		ManufacturedTime:     time.Now,
+		ManufacturedLocation: manufacturedLocation,
+		Make:                 carMake,
+		Model:                model,
 	}
 }
 
 func NewCarStuttgart(carMake string, model string, manufactured func() time.Time) *Car {
 	return &Car{
-		Manufactured: manufactured,
-		Make:         carMake,
-		Model:        model,
+		ManufacturedTime:     manufactured,
+		ManufacturedLocation: "Stuttgart",
+		Make:                 carMake,
+		Model:                model,
 	}
 }
 
