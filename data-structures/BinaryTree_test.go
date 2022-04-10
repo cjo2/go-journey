dfaed8d4ebcda179c09c11e6c4672900967b69cd
Christopher Docuyanan <christophejd@gmail.com>
2022-04-10 13:19:42 -0700

    test cleanup for binary tree

--- data-structures/BinaryTree_test.go
+++ data-structures/BinaryTree_test.go
@@ -1,15 +1,13 @@
 package data_structures
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestBinaryTreeNode_InsertNode(t *testing.T) {
 	head := &BinaryTreeNode{Value: 2}
-	zero := &BinaryTreeNode{Value: 0}
-	head.InsertNode(zero)
+	head.InsertNode(&BinaryTreeNode{Value: 0})
 	head.InsertNode(&BinaryTreeNode{Value: 4})
 	head.InsertNode(&BinaryTreeNode{Value: -1})
 	head.InsertNode(&BinaryTreeNode{Value: 1})
@@ -22,9 +20,6 @@ func TestBinaryTreeNode_InsertNode(t *testing.T) {
 	assert.Equal(t, 4, head.Right.Value)
 	assert.Equal(t, 3, head.Right.Left.Value)
 	assert.Equal(t, 5, head.Right.Right.Value)
-
-	fmt.Printf("%v %v\n", zero, head.Left)
-	fmt.Printf("%v %v", *zero, *head.Left)
 
 	err := head.InsertNode(&BinaryTreeNode{Value: 2})
 	assert.Error(t, err)
